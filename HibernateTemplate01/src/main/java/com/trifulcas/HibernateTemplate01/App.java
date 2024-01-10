6b71cca68bcc07d11ad04fd23e25d6d0e40c43e1
juanpablofuentes <incidencias@gmail.com>
2024-01-10 16:10:43 +0100

    Leer actores

--- HibernateTemplate01/src/main/java/com/trifulcas/HibernateTemplate01/App.java
+++ HibernateTemplate01/src/main/java/com/trifulcas/HibernateTemplate01/App.java
@@ -1,5 +1,16 @@
 package com.trifulcas.HibernateTemplate01;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.trifulcas.models.Actor;
+
 /**
  * Hello world!
  *
@@ -8,6 +19,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+
+		SessionFactory factory = meta.getSessionFactoryBuilder().build();
+		Session session = factory.openSession();
+		try {
+			Actor penelope = session.get(Actor.class, 1);
+			
+			System.out.println(penelope.getFirstName() + " " + penelope.getLastName());
+			List<Actor> lista = session.createQuery("from Actor a where a.first_name like '%ar%' ").getResultList();
+			for (Actor a : lista) {
+				System.out.println(a);
+			}
+			
+			
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		session.close();
+		factory.close();
     }
 }
