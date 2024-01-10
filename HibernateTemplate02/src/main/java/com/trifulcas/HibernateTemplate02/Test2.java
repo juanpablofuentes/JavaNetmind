b694878c44f8352f689804ea4419fd37d940d61b
juanpablofuentes <incidencias@gmail.com>
2024-01-10 16:26:55 +0100

    Test

---
+++ HibernateTemplate02/src/main/java/com/trifulcas/HibernateTemplate02/Test2.java
@@ -0,0 +1,21 @@
+package com.trifulcas.HibernateTemplate02;
+
+import org.hibernate.Session;
+
+import com.trifulcas.hibernate.HibernateUtil;
+import com.trifulcas.models.Actor;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			Actor penelope = session.get(Actor.class, 1);
+			System.out.println(penelope.getFirstName() + " " + penelope.getLastName());
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		session.close();
+	}
+
+}
