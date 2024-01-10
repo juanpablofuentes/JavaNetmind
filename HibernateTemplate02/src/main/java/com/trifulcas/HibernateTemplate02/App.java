bef9886041a133bea751449da44081e63016d37d
juanpablofuentes <incidencias@gmail.com>
2024-01-10 16:18:40 +0100

    First

---
+++ HibernateTemplate02/src/main/java/com/trifulcas/HibernateTemplate02/App.java
@@ -0,0 +1,13 @@
+package com.trifulcas.HibernateTemplate02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
