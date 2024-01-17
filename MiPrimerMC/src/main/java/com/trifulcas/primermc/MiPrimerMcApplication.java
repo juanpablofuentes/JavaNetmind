f6b318b8165e762d19befdc7dec9dfc71296b1eb
juanpablofuentes <incidencias@gmail.com>
2024-01-17 21:37:27 +0100

    a

---
+++ MiPrimerMC/src/main/java/com/trifulcas/primermc/MiPrimerMcApplication.java
@@ -0,0 +1,13 @@
+package com.trifulcas.primermc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerMcApplication.class, args);
+	}
+
+}
---
+++ MiPrimerSpring/src/test/java/com/trifulcas/MiPrimerSpring/MiPrimerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trifulcas.MiPrimerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
