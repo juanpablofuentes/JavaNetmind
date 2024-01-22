1b6aef5ba81dc602661f1bb4787e3cfc2e799c2c
juanpablofuentes <incidencias@gmail.com>
2024-01-22 21:11:50 +0100

    REST

---
+++ SpringBootRest/src/test/java/com/trifulcas/SpringBootRest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trifulcas.SpringBootRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
